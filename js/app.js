2189f3805f654a733c2b48c7dfb7ff7b43ce5fda
anna-balan <dp101292baa@gmail.com>
2016-02-12 23:38:27 +0200

    first

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-$(document).foundation();
+;(function($){
+
+ 	$(function(){
+  		var slider = $('.ba-slider');
+
+  		slider.slick({
+   			dots: false,
+   			infinite:false,
+			arrow:true
+
+  		});
+
+
+ 	});
+
+ 	$(function(){
+  		var sliderFeatured = $('.ba-featured-slider');
+
+  		sliderFeatured.slick({
+   			dots: true,
+   			infinite:false,
+			arrow:false,
+			prevArrow: false,
+			nextArrow:false
+
+  		});
+
+
+ 	});
+
+})(jQuery);
+
